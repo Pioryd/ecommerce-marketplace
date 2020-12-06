3a558ba5c6d9f633b8759af7d93bb3cb92832287
Piotr Rydwański <pioryd123@gmail.com>
2020-12-06 13:27:30 +0100

    Update footer

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -4,10 +4,12 @@ import "./index.scss";
 
 function Footer() {
   return (
-    <div className="footer">
-      <label>Open source project.</label>
-      <a href="https://github.com/Pioryd/ecommerce">Github</a>
-    </div>
+    <React.Fragment>
+      <div className="footer">
+        <label>Open source project.</label>
+        <a href="https://github.com/Pioryd/ecommerce">Github</a>
+      </div>
+    </React.Fragment>
   );
 }
 export default Footer;
